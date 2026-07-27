12e9b44fce061dada4e053edc2ad39c8e0d242cd
myunghoon3847-maker <myunghoon3847@gmail.com>
2026-07-28 02:53:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lumen-ai-v1.3';
+const CACHE='lumen-ai-v140.3';
 const ASSETS=['/','/index.html','/manifest.webmanifest','/lumen-ai-logo.svg','/icons/icon-192.png','/icons/icon-512.png','/icons/apple-touch-icon.png','/favicon.ico'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
